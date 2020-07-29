cec0e95139177c5dcc440cd4943abe7c914f063d
Sneha <sneha46749@gmail.com>
2020-07-30 00:14:32 +0530

    Implementing case sensitivity that is converting the word to lower case

---
+++ Case-sensitivity.py
@@ -0,0 +1,13 @@
+import json 
+
+data=json.load(open("data.json"))
+
+def translate(w):  #word is a local variable passed as a parameter in the function
+    w = w.lower()
+    if w in data:
+        return data[w]  
+    else:
+        return "The word does not exist. Please check it again." 
+
+word = input("Enter Word : ")  #word is a global variable
+print (translate(word))
