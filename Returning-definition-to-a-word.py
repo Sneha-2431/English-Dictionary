40c3bab97e591591fd4fd5efb42049fe5cb7f07f
Sneha <sneha46749@gmail.com>
2020-07-29 23:35:04 +0530

    Function returning definition of the word entered by user

--- Returning-definition-to-a-word.py
+++ Returning-definition-to-a-word.py
@@ -2,8 +2,8 @@ import json
 
 data=json.load(open("data.json"))
 
-def translate(word):  #word is a local variable passed as a parameter in the function
-    return data(word)
+def translate(w):  #word is a local variable passed as a parameter in the function
+    return data(w)
 
 word = input("Enter Word : ")  #word is a global variable
 print (translate(word))
